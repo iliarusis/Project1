67efc5ae334dbbdb8ce2db1826ecb1c353ad74ff
iliarusis <158061213+iliarusis@users.noreply.github.com>
2024-01-31 18:42:39 +0300

    Update Main(num3)

--- Lesson37/Main.cpp
+++ Lesson37/Main.cpp
@@ -4,8 +4,8 @@ int sum(int num1, int num2);
 
 using namespace std;
 int main() {
-	int num1, num2;
-	cin >> num1 >> num2;
-	cout << sum(num1, num2);
+	int num1, num2, num3;
+	cin >> num1 >> num2 >> num3;
+	cout << sum(num1, num2, num3);
 	return 0;
 }
