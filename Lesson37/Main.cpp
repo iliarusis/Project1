5db9455ac155c33786e9d81fcad8b09c0bac555b
iliarusis <iliarusis2@gmail.com>
2024-01-31 18:36:08 +0300

    Update project

--- Lesson37/Main.cpp
+++ Lesson37/Main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+int sum(int num1, int num2);
+
 using namespace std;
 int main() {
-	cout << "<>";
+	int num1, num2;
+	cin >> num1 >> num2;
+	cout << sum(num1, num2);
 	return 0;
 }
